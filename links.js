dcc232586d3256917960d489eac1e775c0ac2b51
ariellimon <166950089+ariellimon@users.noreply.github.com>
2024-04-13 21:40:15 +0200

    Update links.js

--- links.js
+++ links.js
@@ -1,9 +1,14 @@
-console.log("running before 1");
-const links = document.querySelectorAll("#site_menu .set-link a");
-console.log("found links", links);
-links.forEach(element => {
-  console.log("element", element);
-  const href = element.href;
-  element.setAttribute("data-tags", href);
-  console.log("updated link data-tags", href);
-});
+console.log("running before 2");
+const button = document.getElementById("site_menu_button");;
+button.addEventListener("click", () => {
+  setTimeout(() => {
+    const links = document.querySelectorAll("#site_menu .set-link a");
+    console.log("found links", links);
+    links.forEach(element => {
+      console.log("element", element);
+      const href = element.href;
+      element.setAttribute("data-tags", href);
+      console.log("updated link data-tags", href);
+    });
+  }, 200)
+})
