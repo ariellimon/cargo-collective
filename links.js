ace9e33b25b1da143e4d9f620434fbb0f1d130c5
ariellimon <166950089+ariellimon@users.noreply.github.com>
2024-04-13 21:31:41 +0200

    Update links.js

--- links.js
+++ links.js
@@ -1,6 +1,8 @@
-console.log("running before");
+console.log("running before 1");
 const links = document.querySelectorAll("#site_menu .set-link a");
+console.log("found links", links);
 links.forEach(element => {
+  console.log("element", element);
   const href = element.href;
   element.setAttribute("data-tags", href);
   console.log("updated link data-tags", href);
